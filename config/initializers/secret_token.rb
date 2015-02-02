796f6c5076df20967830c49aeadb19e632b840e2
Alan Ridlehoover <alan@ridlehoover.com>
2015-02-01 22:30:35 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Commuter::Application.config.secret_key_base = '2b50abbc2d601c49058ac8cfc34315cfebbb71800c5efe6a16479cf67da2e26b96732086b5dbbf2cde2ca5cc9d37cdbd95c0a46ced1261dd1e99aa85cb578999'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Commuter::Application.config.session_store :cookie_store, key: '_commuter_session'
