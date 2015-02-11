ede61346083aa6018fd310ddcd2d78860731ea9f
Alan Ridlehoover <alan@ridlehoover.com>
2015-02-10 22:09:37 -0800

    Removes expectations that fail at night

--- features/step_definitions/departure_steps.rb
+++ features/step_definitions/departure_steps.rb
@@ -4,8 +4,6 @@ end
 
 Then(/^I should see the list of departures$/) do
   expect(page).to have_content('Pittsburg/Bay Point')
-  expect(page).to have_content('Fremont')
-  expect(page).to have_content('Richmond')
   expect(page).to have_content('Dublin/Pleasanton')
   expect(page).to have_content('Daly City')
 end
